2c812e965fe22d4397dbf7056b8fd79898e0f297
Dawid Zajac <zajdawid@protonmail.com>
2022-12-16 20:58:17 +0100

    refactor: getEventStyles function

--- lib/components/TimelineEvents.tsx
+++ lib/components/TimelineEvents.tsx
@@ -1,24 +1,23 @@
 import type { CSSProperties } from 'react';
 import type { TimelineEvent } from 'lib/types';
-import { useOffsetCalculator, useSizeCalculator } from 'lib/hooks';
 import { useTimelineContext } from 'lib/components/TimelineProvider';
+import offsetCalculator from 'lib/utils/offsetCalculator';
+import sizeCalculator from 'lib/utils/sizeCalculator';
 
 interface TimelineEventsProps {
   render: (props: { getEventStyles: (evt: TimelineEvent) => CSSProperties }) => JSX.Element | null;
 }
 
 export default function TimelineEvents({ render }: TimelineEventsProps) {
-  const { direction } = useTimelineContext();
-  const calcOffset = useOffsetCalculator();
-  const calcSize = useSizeCalculator();
+  const timeline = useTimelineContext();
 
-  const isHorizontalTimeline = direction === 'horizontal';
+  const calcOffset = offsetCalculator(timeline);
+  const calcSize = sizeCalculator(timeline);
 
   const getEventStyles = (evt: TimelineEvent): CSSProperties => ({
     position: 'absolute',
-    ...(isHorizontalTimeline
-      ? { width: calcSize(evt), left: calcOffset(evt.startDate) }
-      : { height: calcSize(evt), top: calcOffset(evt.startDate) }),
+    ...calcOffset(evt.startDate),
+    ...calcSize(evt),
   });
 
   return render({ getEventStyles });
--- lib/components/indicators/TimelineTimeIndicator.tsx
+++ lib/components/indicators/TimelineTimeIndicator.tsx
@@ -1,20 +1,19 @@
 import type { CSSProperties } from 'react';
-import { useOffsetCalculator } from 'lib/hooks';
 import { useTimelineContext } from 'lib/components/TimelineProvider';
+import offsetCalculator from 'lib/utils/offsetCalculator';
 
 interface TimelineTimeIndicatorProps {
   render: (props: { getIndicatorStyles: (date: Date) => CSSProperties }) => JSX.Element | null;
 }
 
 export default function TimelineTimeIndicator({ render }: TimelineTimeIndicatorProps) {
-  const { direction } = useTimelineContext();
-  const calcOffset = useOffsetCalculator();
+  const timeline = useTimelineContext();
 
-  const isHorizontalTimeline = direction === 'horizontal';
+  const calcOffset = offsetCalculator(timeline);
 
   const getIndicatorStyles = (date: Date): CSSProperties => ({
     position: 'absolute',
-    ...(isHorizontalTimeline ? { left: calcOffset(date) } : { top: calcOffset(date) }),
+    ...calcOffset(date),
   });
 
   return render({ getIndicatorStyles });
